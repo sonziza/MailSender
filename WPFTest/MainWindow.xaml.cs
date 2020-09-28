1aec5744f4efc9edd939b1ad59adcf8baf310ff9
Oleg <sonziza@yandex.ru>
2020-09-28 16:09:14 +0300

    Косметика. В окно вынесли остальные поля для ввода

--- WPFTest/MainWindow.xaml.cs
+++ WPFTest/MainWindow.xaml.cs
@@ -5,6 +5,15 @@ using System.Net.Mail;
 
 namespace WPFTest
 {
+
+    public static class ConfData
+    {
+        public const string sender_mail = "sonziza@yandex.ru";
+        public const string sender_name = "Oleg";
+        public const string recipient_mail = "sonziza159@gmail.com";
+        public const string recipient_name = "Olejoint";
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -18,14 +27,14 @@ namespace WPFTest
         private void BtnSend_Click(object sender, RoutedEventArgs e)
         {
             //указываем адреса эл почты отправителя/получателя
-            var mail_sender = new MailAddress("sonziza@yandex.ru", "Oleg");
-            var recipient = new MailAddress("sonziza159@gmail.com", "Olejoint");
+            var mail_sender = new MailAddress(ConfData.sender_mail, ConfData.sender_name);
+            var recipient = new MailAddress(ConfData.recipient_mail, ConfData.recipient_name);
 
             //создаем обьект сообщения отправителя/получателя
             using var message = new MailMessage(mail_sender, recipient)
             {
-                Subject = "Это тестовое сообщение",
-                Body = "Проверка тестового сообщения. Просьба не отвечать на него!",
+                Subject = txtTitle.Text,
+                Body = txtBody.Text,
             };
 
             //Создадим клиента-отправителя для общения с почтовым сервером по протоколу SMTP
@@ -42,7 +51,10 @@ namespace WPFTest
                 //Поэтому добавим информацию с нашими учетными данными
                 client.Credentials = new NetworkCredential(login, password);
                 //Работать будем по SSL шифрованию данных
-                client.EnableSsl = true;
+                if (CheckSSL.IsChecked != null)
+                {
+                    client.EnableSsl = (bool)CheckSSL.IsChecked;
+                }
                 //теперь клиенту даем доступ к открытию канала с сервером.
                 //Отправляем сообщение
                 client.Send(message);
