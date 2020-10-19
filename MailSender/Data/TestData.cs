ec4089d1c80f1807927dc53946501b1b66b1c916
Oleg <sonziza@yandex.ru>
2020-10-19 15:47:55 +0300

    Начало прописания логики данных

---
+++ MailSender/Data/TestData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.Data
+{
+    public static class TestData
+    {
+        //public static Dictionary<string, string> Senders
+        //{
+        //    get { return }
+        //}
+        //public static Dictionary<string,string> dicSenders = new Dictionary<string, string>()
+        //{
+        //    {"sonziza" }
+        //}
+    }
+}
