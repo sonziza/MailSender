9d1b198d2ebe7b44b2e5fd700abc95d3cbf9f927
Oleg <sonziza@yandex.ru>
2020-10-19 23:29:41 +0300

    привязка данных к компонентам + инициализация библиотеки MailSender.lib

---
+++ MailSender.lib/MailSenderService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace MailSender.lib
+{
+    class MailSenderService
+    {
+        public string ServerAddress { get; set; }
+
+        public int ServerPort { get; set; }
+
+        public bool UseSSL { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public void SendMessage(string SenderAddress, string RecipientAddress, string Subject, string Body)
+        {
+            var from = new MailAddress(SenderAddress, "Oleg");
+            var to = new MailAddress(RecipientAddress, "Olejoint");
+
+            using (var message = new MailMessage(from, to))
+            {
+
+                message.Subject = Subject;
+                message.Body = Body;
+
+                using (var client = new SmtpClient(ServerAddress, ServerPort))
+                {
+                    client.EnableSsl = UseSSL;
+
+                    client.Credentials = new NetworkCredential
+                    {
+                        UserName = Login,
+                        Password = Password
+                    };
+
+                    try
+                    {
+                        client.Send(message);
+                    }
+                    catch (SmtpException e)
+                    {
+                        Trace.TraceError(e.ToString());
+                        throw;
+                    }
+                }
+            }
+        }
+
+    }
+}
