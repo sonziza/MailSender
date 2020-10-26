a868eaa16da3bd194306609836acd6b7fee59f46
Oleg <sonziza@yandex.ru>
2020-10-26 22:10:17 +0300

    Пример работы ViewModel-View

---
+++ WPFTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPFTest.ViewModels.Base;
+
+namespace WPFTest.ViewModels
+{
+    class MainWindowViewModel:ViewModel
+    {
+        private string _Title = "Главное окно программы";
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+    }
+}
