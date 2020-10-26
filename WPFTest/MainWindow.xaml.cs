67795e635bdbfc924345b8604e66a2ed8849e90f
Oleg <sonziza@yandex.ru>
2020-10-26 21:59:33 +0300

    Привязка к данным (интерфейс)

--- WPFTest/MainWindow.xaml.cs
+++ WPFTest/MainWindow.xaml.cs
@@ -6,11 +6,6 @@ using System.Net.Mail;
 namespace WPFTest
 {
 
-    public static class ConfData
-    {
-        public const int server_port = 25;
-        public const string server_address = "smtp.yandex.ru";
-    }
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -22,41 +17,6 @@ namespace WPFTest
             InitializeComponent();
         }
 
-        private void BtnSend_Click(object Sender, RoutedEventArgs e)
-        {
-            EmailSendService service = new EmailSendService() { 
-                Login = TxtLogin.Text, 
-                Password = TxtPass.Password,
-                ServerAddress = ConfData.server_address,
-                ServerPort = ConfData.server_port, 
-                UseSSL = (bool)CheckSSL.IsChecked, 
-            };
-            Models.Sender sender = new Models.Sender()
-            {
-                Name = TxtLogin.Text,
-                Address = TxtSenderMail.Text
-            };
-            Models.Recipient recipient = new Models.Recipient()
-            {
-                Name = TxtRecipient.Text,
-                Address = TxtRecipMail.Text
-            };
-            Models.Message message = new Models.Message()
-            {
-                Title = txtTitle.Text,
-                Body = txtBody.Text
-            };
-
-            try
-            {
-                service.SendMessage(sender.Address, recipient.Address, message.Title, message.Body);
-            }
-            catch (SmtpException error)
-            {
-                MessageBox.Show(
-                    "Ошибка при отправке почты " + error.Message, "Ошибка",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }    
+           
     }
 }
