19e25cf12f442b2af029b87246c0371a45874c37
Oleg <sonziza@yandex.ru>
2020-10-30 14:34:23 +0300

    Добавление функционала Hosting

--- MailSender/App.xaml.cs
+++ MailSender/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MailSender.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +16,21 @@ namespace MailSender
     /// </summary>
     public partial class App : Application
     {
+        private static IHost _Hosting;
+        public static IHost Hosting => _Hosting ??= Host.CreateDefaultBuilder(Environment.GetCommandLineArgs())
+            .ConfigureServices(ConfigureServices)
+            .Build();
+        //обеспечим доступ к контейнеру сервисов
+        public static IServiceProvider Services => Hosting.Services;
+
+        /// <summary>
+        /// здесь хранятся все сервисы, которые понадобятся в нашем приложении
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="services">сервисы</param>
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+        }
     }
 }
