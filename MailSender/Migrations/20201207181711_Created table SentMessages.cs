4cdb41c5a84e06d9bafdf948d8c35649b3496649
Oleg <sonziza@yandex.ru>
2020-12-07 21:25:31 +0300

    Миграция - добавлена таблица SentMessages

---
+++ MailSender/Migrations/20201207181711_Created table SentMessages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MailSender.Migrations
+{
+    public partial class CreatedtableSentMessages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SentMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AddresFrom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AssressTo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateTimeSent = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MessageSubject = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MessageBody = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SentMessages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SentMessages");
+        }
+    }
+}
